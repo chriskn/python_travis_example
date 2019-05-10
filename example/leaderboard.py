e343db73e6cea5ca1b6bd2c06755fba8793a5398
chriskn <christoph.knauf@t-online.de>
2019-05-10 12:36:16 +0200

    black

--- example/leaderboard.py
+++ example/leaderboard.py
@@ -7,19 +7,16 @@ import os
 import numpy as np
 import pandas as pd
 
-RESULTS = pd.DataFrame([
-    ['tom', 3],
-    ['nick', 5],
-    ['juli', 19],
-    ['tom', 8],
-], columns=['Name', 'Points'])
+RESULTS = pd.DataFrame(
+    [["tom", 3], ["nick", 5], ["juli", 19], ["tom", 8]], columns=["Name", "Points"]
+)
 
 
 def calculate_leaderboard(data):
     """Caluclates Leaderboard"""
-    board = data.groupby(['Name'], as_index=False).sum()
-    board.sort_values('Points', ascending=False, inplace=True)
-    board.index = np.arange(1, len(board)+1)
+    board = data.groupby(["Name"], as_index=False).sum()
+    board.sort_values("Points", ascending=False, inplace=True)
+    board.index = np.arange(1, len(board) + 1)
     board.index.name = "Rank"
     return board
 
@@ -31,7 +28,7 @@ def write_leaderboard(data, filepath, sheetname):
     writer.save()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # disable invalid name check
     # pylint: disable = C0103
     leaderboard = calculate_leaderboard(RESULTS)
--- example/test/test_leaderboard.py
+++ example/test/test_leaderboard.py
@@ -10,26 +10,29 @@ from example import leaderboard
 
 class TestLeaderboard(unittest.TestCase):
 
-    test_data = pd.DataFrame([
-        ['tom', 10],
-        ['nick', 15],
-        ['juli', 14],
-        ['tom', 3],
-        ['nick', 15],
-        ['juli', 20],
-        ['peter', 20],
-        ['peter', 20]
-    ], columns=['Name', 'Points'])
+    test_data = pd.DataFrame(
+        [
+            ["tom", 10],
+            ["nick", 15],
+            ["juli", 14],
+            ["tom", 3],
+            ["nick", 15],
+            ["juli", 20],
+            ["peter", 20],
+            ["peter", 20],
+        ],
+        columns=["Name", "Points"],
+    )
 
     def test_leaderboard_groups_by_name(self):
         result = leaderboard.calculate_leaderboard(self.test_data)
-        self.assertEqual(set(result['Name']), set(["tom", "nick", "juli", "peter"]))
+        self.assertEqual(set(result["Name"]), set(["tom", "nick", "juli", "peter"]))
 
     def test_leaderboard_sums_up_by_name(self):
         result = leaderboard.calculate_leaderboard(self.test_data)
-        self.assertEqual(set(result['Points']), set([13, 34, 40, 30]))
+        self.assertEqual(set(result["Points"]), set([13, 34, 40, 30]))
 
     def test_leaderboard_is_sorted_by_points(self):
         result = leaderboard.calculate_leaderboard(self.test_data)
-        self.assertEqual(list(result['Name']), ["peter", "juli", "nick", "tom"])
-        self.assertEqual(list(result['Points']), [40, 34, 30, 13])
+        self.assertEqual(list(result["Name"]), ["peter", "juli", "nick", "tom"])
+        self.assertEqual(list(result["Points"]), [40, 34, 30, 13])
